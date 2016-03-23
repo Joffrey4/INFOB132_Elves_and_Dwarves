148e3676d865da5f43f728c55c065f2d6fabdf29
Joffrey Bienvenu <joffreybvn@gmail.com>
2016-03-23 17:41:38 +0100

    Work properly. Return a list with the architecture of the ui. Ready to print.

--- core/create_data_ui.py
+++ core/create_data_ui.py
@@ -2,48 +2,59 @@
 from colorama import Fore, Back, Style
 
 
-def create_data_ui(map_size):
+def create_data_ui(data_map):
+    data_ui = [[]] * (17 + data_map['map_size'])
 
-    data_ui = []
-    #Conditionne la taille du tableau à 10 lignes minimum -> Laisse de l'espace pour les statistiques.
-    min_size = 10
-    if map_size >= min_size:
-        min_size = map_size
+    # Initialisation of the displaying constants.
+    grid_size = 5 * data_map['map_size']
+    ui_color = '{1}'
 
-    #cell_size, multiplicateur qui permet d'allonger les cellules du damier.
-    cell_sizer = ((map_size -1)/ 10) +1
+    line_coloured = ui_color + ('█' * 130) + Style.RESET_ALL
+    border_black = Back.BLACK + '  ' + Style.RESET_ALL
 
-    #board_width, longueur (en caractères) du damier.
-    board_width = (3 + cell_sizer) * map_size
+    border_coloured_left = ui_color + ('█' * 9) + Style.RESET_ALL
+    border_coloured_right = ui_color + ('█' * 9) + Style.RESET_ALL
+    border_coloured_middle = ui_color + (' ' * 8) + Style.RESET_ALL
 
-    #Initialisation des variables d'affichage.
-    LINE_COLORED = ui_color + ('█' * 130) + Style.RESET_ALL
-    LINE_BLACK = Back.BLACK + ' ' * (board_width + 9) + Style.RESET_ALL
-    BORDER_COLORED = ui_color + '███' + ('█' * 2 * cell_sizer) + Style.RESET_ALL
-    BORDER_BLACK = Back.BLACK + '  ' + Style.RESET_ALL
-    LINE_FILL = ui_color + (127 - (board_width + 12 + (4 * cell_sizer))) * '█' + Style.RESET_ALL
-    LINE_BLANK = ui_color + (127 - (board_width + 35 + (4 * cell_sizer))) * '█' + Style.RESET_ALL
+    border_white = ' ' * 4
 
-    #Enregistrement - haut de l'ui, lignes colorées.
+    # Generate and save the top of the UI.
     for i in range(3):
-        data_ui[i] =  ui_color + ('█' * 130) + Style.RESET_ALL
+        data_ui[i] = line_coloured
 
-    #Enregistrement - haut de l'ui, bord supérieur des cadres.
-    turn_message = 'Tour %s - %s, à vous de jouer ! %s'
-    colored_turn_line = Fore.WHITE + Back.BLACK + turn_message + Style.RESET_ALL + Back.BLACK + ' ' * (board_width + 7 - len(turn_message)) + Style.RESET_ALL
-    colored_stats_line = BORDER_BLACK + Fore.WHITE + Back.BLACK + 'Statistiques:' + Style.RESET_ALL + Back.BLACK + (' ' * 5) + Style.RESET_ALL + BORDER_BLACK
-    data_ui[3] = BORDER_COLORED + BORDER_BLACK + colored_turn_line + BORDER_BLACK + BORDER_COLORED + colored_stats_line + LINE_FILL
+    # Generate and save the top of the grid.
+    turn_message = 'Turn {2} - {3}, it\'s up to you ! {4}'
+    data_ui[3] = border_coloured_left + Fore.WHITE + Back.BLACK + '  ' + turn_message + '  ' + Style.RESET_ALL + border_coloured_right
+    data_ui[4] = border_coloured_left + border_black + ' ' * (grid_size + 6) + border_black + border_coloured_right
 
-    #Enregistrement - haut du damier et titre "Elves".
-    elves_subtitle = 'Elves:' + ' ' * 13
-    data_ui[4] = BORDER_COLORED + BORDER_BLACK + ' ' * (board_width +5) + BORDER_BLACK + BORDER_COLORED + BORDER_BLACK + elves_subtitle + BORDER_BLACK + LINE_FILL
+    # Generate and save the architecture of the grid.
+    for line in range(1, data_map['map_size'] + 1):
+        data_ui[line + 5] = border_coloured_left + border_black + Fore.BLACK + ('0' + str(line))[-2:] + ' ' + Style.RESET_ALL + '{2}' + '   ' + border_black + border_coloured_right
 
-    #Enregistrement - damier
-    for line in range(1, min_size + 1):
-        if line <= map_size:
-            data_ui[line + 5] = BORDER_COLORED + BORDER_BLACK + Fore.BLACK + ('0' + str(line))[-2:] + ' ' + Style.RESET_ALL
-            data_ui[line + 5] += '%s  ' + BORDER_BLACK + BORDER_COLORED
+    # Generate and save the foot of the grid.
+    data_ui[data_map['map_size'] + 5] = border_coloured_left + border_black + Fore.BLACK + '   '
+    for i in range(1, data_map['map_size'] + 1):
+        data_ui[data_map['map_size'] + 5] += '  ' + ('0' + str(i))[-2:] + ' '
+    data_ui[data_map['map_size'] + 5] += '  ' + border_black + border_coloured_right
 
-        if line <= 10:
+    data_ui[data_map['map_size'] + 6] = border_coloured_left + Back.BLACK + (grid_size + 10) * ' ' + Style.RESET_ALL + border_coloured_right
+
+    # Generate and save the top of the statistics.
+    data_ui[data_map['map_size'] + 7] = line_coloured
+
+    data_ui[data_map['map_size'] + 8] = border_coloured_left + Fore.WHITE + Back.BLACK + '  Your units:' + (' ' * 39) + Style.RESET_ALL + border_coloured_middle
+    data_ui[data_map['map_size'] + 8] = Fore.WHITE + Back.BLACK + '  Opponent\'s units:' + (' ' * 33) + Style.RESET_ALL + border_coloured_right
+
+    # Generate and save the content of the statistics.
+    for i in range(4):
+        data_ui[data_map['map_size'] + 9 + i] = border_coloured_left + border_black + border_white + '{2}' + border_white + '{3}' + border_white + border_black + border_coloured_middle
+        data_ui[data_map['map_size'] + 9 + i] += border_black + border_white + '{4}' + border_white + '{5}' + border_white + border_black + border_coloured_right
+
+    # Generate and save the foot of the statistics.
+    data_ui[data_map['map_size'] + 13] = border_coloured_left + Back.BLACK + (' ' * 50) + Style.RESET_ALL + border_coloured_middle
+    data_ui[data_map['map_size'] + 13] = Back.BLACK + (' ' * 50) + Style.RESET_ALL + border_coloured_right
+
+    for i in range(2):
+        data_ui[data_map['map_size'] + 14 + i] = line_coloured
 
     return data_ui
