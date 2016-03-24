42a23488b8b1188e336c89809535f23bfe1f2b79
Dropshift <jonathan.maroit@student.unamur.be>
2016-03-24 07:58:53 +0100

    Some modification and little factorisation

--- ia/ia_action.py
+++ ia/ia_action.py
@@ -24,6 +24,21 @@ def ia_action(player,enemy, data_map):
         unit_type = data_map[player][units][0]
         enable_move_x=[0]
         enable_move_y=[0]
+        if int(start_x_pos) == 1:
+            enable_move_x.append(1)
+        elif int(start_x_pos) == data_map['map_size']:
+            enable_move_x.append(-1)
+        else:
+            enable_move_x.append(-1)
+            enable_move_x.append(1)
+
+        if int(start_y_pos) == 1:
+            enable_move_y.append(1)
+        elif int(start_y_pos) == data_map['map_size']:
+            enable_move_y.append(-1)
+        else:
+            enable_move_y.append(-1)
+            enable_move_y.append(1)
 
 
         if unit_type == 'E':
@@ -37,21 +52,6 @@ def ia_action(player,enemy, data_map):
                     command += str(start_x_pos) + '_' + str(start_y_pos) + ' ' + action_type[0] + ('0'+str(target[0]))[-2:] + '_' + ('0'+str(target[1]))[-2:]+ '   '
 
             if units not in used_units:
-                if int(start_x_pos) == 1:
-                    enable_move_x.append(1)
-                elif int(start_x_pos) == data_map['map_size']:
-                    enable_move_x.append(-1)
-                else:
-                    enable_move_x.append(-1)
-                    enable_move_x.append(1)
-
-                if int(start_y_pos) == 1:
-                    enable_move_y.append(1)
-                elif int(start_y_pos) == data_map['map_size']:
-                    enable_move_y.append(-1)
-                else:
-                    enable_move_y.append(-1)
-                    enable_move_y.append(1)
 
                 target_x_pos = int(start_x_pos)+enable_move_x[random.randint(0, (len(enable_move_x)-1))]
                 target_y_pos = int(start_y_pos)+enable_move_y[random.randint(0, (len(enable_move_y)-1))]
@@ -70,33 +70,15 @@ def ia_action(player,enemy, data_map):
                     command += str(start_x_pos) + '_' + str(start_y_pos) + ' ' + action_type[0] + ('0'+str(target[0])[-2:] + '_' + ('0'+str(target[1]))[-2:] + '   '
 
             if units not in used_units:
-                if int(start_x_pos) == 1:
-                    enable_move_x.append(1)
-                elif int(start_x_pos) == data_map['map_size']:
-                    enable_move_x.append(-1)
-                else:
-                    enable_move_x.append(-1)
-                    enable_move_x.append(1)
 
-                if int(start_y_pos) == 1:
-                    enable_move_y.append(1)
-                elif int(start_y_pos) == data_map['map_size']:
-                    enable_move_y.append(-1)
-                else:
-                    enable_move_y.append(-1)
-                    enable_move_y.append(1)
                 target_x_pos = int(start_x_pos)+enable_move_x[random.randint(0, (len(enable_move_x)-1))]
-                print units,target_x_pos,int(start_x_pos)
                 if target_x_pos == int(start_x_pos):
                     target_y_pos = int(start_y_pos)+enable_move_y[random.randint(0, (len(enable_move_y)-1))]
                 if (target_x_pos,target_y_pos) not in data_map[player] and (target_x_pos,target_y_pos) not in data_map[enemy]:
                     print 'done'
                     command += str(start_x_pos) + '_' + str(start_y_pos) + ' ' + action_type[1] +(('0'+str(target_x_pos))[-2:]) + '_' + (('0'+str(target_y_pos))[-2:]) + '   '
 
-
-
-
-    print command
     return command
+
 
 
