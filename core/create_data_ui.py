ad509b361af370dad9e8c24c82228113ac4fa1fa
Joffrey Bienvenu <joffreybvn@gmail.com>
2016-04-10 19:39:04 +0200

    display_map and create_data_ui fit now for any size of map.

--- core/create_data_ui.py
+++ core/create_data_ui.py
@@ -28,6 +28,10 @@ def create_data_ui(data_map):
     line_coloured = ui_color + ('█' * 130) + Style.RESET_ALL
     border_black = Back.BLACK + '  ' + Style.RESET_ALL
 
+    margin_left = ((20 - data_map['map_size']) * 5) / 2
+    margin_right = ((20 - data_map['map_size']) * 5) - (((20 - data_map['map_size']) * 5) / 2)
+    border_coloured_margin_left = ui_color + ('█' * (9 + margin_left)) + Style.RESET_ALL
+    border_coloured_margin_right = ui_color + ('█' * (9 + margin_right)) + Style.RESET_ALL
     border_coloured_left = ui_color + ('█' * 9) + Style.RESET_ALL
     border_coloured_right = ui_color + ('█' * 9) + Style.RESET_ALL
     border_coloured_middle = ui_color + ('█' * 8) + Style.RESET_ALL
@@ -40,23 +44,23 @@ def create_data_ui(data_map):
 
     # Generate and save the top of the grid.
     turn_message = 'Turn %(turn)s - %(playername)s, it\'s up to you ! %(blank)s'
-    data_ui[3] = border_coloured_left + Fore.WHITE + Back.BLACK + '  ' + turn_message + '  ' + Style.RESET_ALL + border_coloured_right
-    data_ui[4] = border_coloured_left + border_black + ' ' * (grid_size + 8) + border_black + border_coloured_right
+    data_ui[3] = border_coloured_margin_left + Fore.WHITE + Back.BLACK + '  ' + turn_message + '  ' + Style.RESET_ALL + border_coloured_margin_right
+    data_ui[4] = border_coloured_margin_left + border_black + ' ' * (grid_size + 8) + border_black + border_coloured_margin_right
 
     # Generate and save the architecture of the grid.
     for i in range(1, data_map['map_size'] + 1):
-        data_ui[i + 4] = border_coloured_left + border_black + Fore.BLACK + ' ' + ('0' + str(i))[-2:] + ' ' + Style.RESET_ALL
+        data_ui[i + 4] = border_coloured_margin_left + border_black + Fore.BLACK + ' ' + ('0' + str(i))[-2:] + ' ' + Style.RESET_ALL
         for j in range(1, data_map['map_size'] + 1):
             data_ui[i + 4] += '%((' + str(i) + ',' + str(j) + '))5s' + Style.RESET_ALL
-        data_ui[i + 4] += '    ' + border_black + border_coloured_right
+        data_ui[i + 4] += '    ' + border_black + border_coloured_margin_right
 
     # Generate and save the foot of the grid.
-    data_ui[data_map['map_size'] + 5] = border_coloured_left + border_black + Fore.BLACK + '   '
+    data_ui[data_map['map_size'] + 5] = border_coloured_margin_left + border_black + Fore.BLACK + '   '
     for i in range(1, data_map['map_size'] + 1):
         data_ui[data_map['map_size'] + 5] += '  ' + ('0' + str(i))[-2:] + ' '
-    data_ui[data_map['map_size'] + 5] += '     ' + border_black + border_coloured_right
+    data_ui[data_map['map_size'] + 5] += '     ' + border_black + border_coloured_margin_right
 
-    data_ui[data_map['map_size'] + 6] = border_coloured_left + Back.BLACK + (grid_size + 12) * ' ' + Style.RESET_ALL + border_coloured_right
+    data_ui[data_map['map_size'] + 6] = border_coloured_margin_left + Back.BLACK + (grid_size + 12) * ' ' + Style.RESET_ALL + border_coloured_margin_right
 
     # Generate and save the top of the statistics.
     data_ui[data_map['map_size'] + 7] = line_coloured
