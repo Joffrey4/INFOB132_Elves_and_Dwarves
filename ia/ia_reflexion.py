fcc412f3869350464d6ebec51fcb40ef26443dc1
Joffrey Bienvenu <joffreybvn@gmail.com>
2016-04-29 13:50:33 +0200

    Movement of the ia corrected.

--- ia/ia_reflexion.py
+++ ia/ia_reflexion.py
@@ -19,15 +19,15 @@ def ia_reflexion(data_ia, data_map, player):
     specification: Bienvenu Joffrey & Laurent Emilie v.1 (20/04/16)
     implementation:
     """
-    ia = 'player' + str(data_ia['id'])
-    enemy = 'player' + str((data_ia['id'] % 2) + 1)
+    ia = 'player' + str((data_ia['id'] % 2) + 1)
+    enemy = 'player' + str(data_ia['id'])
     commands = []
 
     unit_has_attacked = 0
     for ia_unit in data_ia[ia]:
         for enemy_unit in data_ia[enemy]:
 
-            # Find each possible target for the Elves.
+            # Find each possible target for the Elves - ATTACK
             unit_targets = []
             if data_ia[ia][ia_unit][0] == 'E':
                 for i in range(2):
@@ -52,17 +52,31 @@ def ia_reflexion(data_ia, data_map, player):
             commands.append([ia_unit, ' -a-> ', target])
             unit_has_attacked += 1
 
-    # Find the weakest of all enemy's units.
-    if not unit_has_attacked:
-        target_list = data_ia[enemy].keys()
-        target = target_list[0]
+        # Find the weakest of all enemy's units - MOVE
+        if not unit_has_attacked:
+            target_list = data_ia[enemy].keys()
+            target = target_list[0]
 
-        for enemy_unit in data_ia[enemy]:
-            if data_ia[enemy][enemy_unit][0] == 'D' or data_ia[enemy][enemy_unit][1] < data_ia[enemy][target][1]:
-                target = enemy_unit
+            for enemy_unit in data_ia[enemy]:
+                if data_ia[enemy][enemy_unit][0] == 'D' or data_ia[enemy][enemy_unit][1] < data_ia[enemy][target][1]:
+                    target = enemy_unit
 
-        # Write the move
-        for ia_unit in data_ia[ia]:
-            commands.append([ia_unit, ' -m-> ', target])
+            target_cell = ia_unit
+            # Move on Y axis
+            if target and abs(ia_unit[1] - target[1]) > abs(ia_unit[0] - target[0]):
+                if ia_unit[1] > target[1]:
+                    target_cell[1] -= 1
+                else:
+                    target_cell[1] += 1
+            # Move on X axis
+            else:
+                if ia_unit[0] > target[0]:
+                    target_cell[1] -= 1
+                else:
+                    target_cell[1] += 1
+
+            # Write the move
+            if target_cell != ia_unit:
+                commands.append([ia_unit, ' -m-> ', target])
 
     return commands
